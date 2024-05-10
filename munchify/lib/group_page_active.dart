a65824e79a71808d9e7b86ae4c7b655073f9cfca
Domme533 <dominikdengler.dev@gmail.com>
2024-05-10 22:03:12 +0200

    Update group_page_active.dart

---
+++ munchify/lib/group_page_active.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GroupPageActive extends StatefulWidget {
+  @override
+  _GroupPageActiveState createState() => _GroupPageActiveState();
+}
+
+class _GroupPageActiveState extends State<GroupPageActive> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Group Page'),
+      ),
+      body: Container(
+        // Add your widgets here
+      ),
+    );
+  }
+}
